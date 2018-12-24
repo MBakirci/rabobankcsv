4dd13f3650140e2d36dd47eeaad202c9e9b347a7
Mehmet Bakirci <m.bakirci@outlook.com>
2018-12-25 00:04:11 +0100

    open Downloads directory as initial

--- src/main/java/javafx/MainController.java
+++ src/main/java/javafx/MainController.java
@@ -1,4 +1,4 @@
-package main;
+package javafx;
 
 import domain.Bankafschrift;
 import javafx.event.ActionEvent;
@@ -14,42 +14,45 @@ import java.io.IOException;
 import java.text.ParseException;
 
 public class MainController {
-    public Button btnOpen;
-    public TextField txtFilePath;
-    public Button btnSave;
-    private CSVReader csvReader = new CSVReader("", ",");
-    private Bankafschrift afschrift;
+  public Button btnOpen;
+  public TextField txtFilePath;
+  public Button btnSave;
+  private CSVReader csvReader = new CSVReader("", ",");
+  private Bankafschrift afschrift;
 
-    public void OnOpen(ActionEvent actionEvent) throws ParseException {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open bankafschrift");
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
-                new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
-        File selectedFile = fileChooser.showOpenDialog(btnOpen.getScene().getWindow());
-        if (selectedFile != null) {
-            this.txtFilePath.setText(selectedFile.getAbsolutePath());
-            this.txtFilePath.end();
-            this.afschrift = csvReader.read(txtFilePath.getText());
-            System.out.print(this.afschrift.toString());
-            this.btnSave.setDisable(false);
-        }
+  public void OnOpen(ActionEvent actionEvent) throws ParseException {
+    FileChooser fileChooser = new FileChooser();
+    String currentUsersHomeDir = System.getProperty("user.home");
+    String downloadsDir = currentUsersHomeDir + File.separator + "Downloads";
+    fileChooser.setInitialDirectory(new File(downloadsDir));
+    fileChooser.setTitle("Open bankafschrift");
+    fileChooser.getExtensionFilters().addAll(
+        new FileChooser.ExtensionFilter("CSV Files", "*.csv"),
+        new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+    File selectedFile = fileChooser.showOpenDialog(btnOpen.getScene().getWindow());
+    if (selectedFile != null) {
+      this.txtFilePath.setText(selectedFile.getAbsolutePath());
+      this.txtFilePath.end();
+      this.afschrift = csvReader.read(txtFilePath.getText());
+      System.out.print(this.afschrift.toString());
+      this.btnSave.setDisable(false);
     }
+  }
 
-    public void OnSave(ActionEvent actionEvent) throws IOException, WriteException {
-        ExcelWriter writer = new ExcelWriter(afschrift);
-        FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Excel files (*.xls)", "*.xls");
-        fileChooser.getExtensionFilters().add(extFilter);
-        File file = fileChooser.showSaveDialog(btnSave.getScene().getWindow());
+  public void OnSave(ActionEvent actionEvent) throws IOException, WriteException {
+    ExcelWriter writer = new ExcelWriter(afschrift);
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Excel files (*.xls)", "*.xls");
+    fileChooser.getExtensionFilters().add(extFilter);
+    File file = fileChooser.showSaveDialog(btnSave.getScene().getWindow());
 
-        if (file != null) {
-            String fileName = file.getAbsolutePath();
-            if (!fileName.endsWith(".xls")) {
-                fileName = fileName + ".xls";
-            }
-            writer.setOutputFile(fileName);
-            writer.write();
-        }
+    if (file != null) {
+      String fileName = file.getAbsolutePath();
+      if (!fileName.endsWith(".xls")) {
+        fileName = fileName + ".xls";
+      }
+      writer.setOutputFile(fileName);
+      writer.write();
     }
+  }
 }
