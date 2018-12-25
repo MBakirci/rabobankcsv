18ceacb96462a04b39d00e2c07b284b8839c0c53
Mehmet Bakirci <m.bakirci@outlook.com>
2018-12-25 22:08:43 +0100

    added icon + updated title :)

--- src/main/java/javafx/Main.java
+++ src/main/java/javafx/Main.java
@@ -4,19 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
-        primaryStage.setTitle("Rabobank CSV parser");
-        primaryStage.setScene(new Scene(root, 400, 275));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+    primaryStage.getIcons().add(new Image("/img/rabobankcsv.png"));
+    primaryStage.setTitle("Rabobank CSV parser 2.0");
+    primaryStage.setScene(new Scene(root, 400, 275));
+    primaryStage.show();
+  }
 }
